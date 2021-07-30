b4eae829d320cc12c9c4cce5b6131c8a8387e287
tapswap <tapswapmoneydex@gmail.com>
2021-07-30 16:27:13 +0800

    wine bar

--- src/views/Pools/components/PoolCard.tsx
+++ src/views/Pools/components/PoolCard.tsx
@@ -124,7 +124,7 @@ const PoolCard: React.FC<HarvestProps> = ({ pool }) => {
       {isFinished && sousId !== 0 && <PoolFinishedSash />}
       <div style={{ padding: '24px' }}>
         <CardTitle isFinished={isFinished && sousId !== 0}>
-          {isOldSyrup && '[OLD]'} {tokenLabel} {TranslateString(999, 'Launchpad')}
+          {isOldSyrup && '[OLD]'} {tokenLabel} {TranslateString(999, 'Winebar')}
         </CardTitle>
         <div style={{ marginBottom: '8px', display: 'flex', alignItems: 'center' }}>
           <div style={{ flex: 1 }}>
